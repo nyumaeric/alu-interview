84ba21a43d4ab02f3e27725e6b98f43b448642bb
nyumaeric <e.nyumajr@alustudent.com>
2024-01-28 15:00:24 +0000

    0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,42 +1,43 @@
-#!/usr/bin/python3
-"""
-0-rain
-"""
+from time import sleep
+from random import randint
+import fcntl, termios, struct
 
-def rain(walls):
-    """
-    Calculate how many square units of water will be retained after it rains.
+def render():
+  print('\033[2J')
+  for r in data:
+    for c in r:
+      v = states[c] if c < len(states) else states[-1]
+      print(v,end='')
+    print('')
 
-    :param walls: List of non-negative integers representing wall heights.
-    :return: Integer indicating total amount of rainwater retained.
-    """
-    if not walls:
-        return 0
+def update():
+  for ir in range(len(data)-1,-1,-1):
+    for ic, c in enumerate(data[ir]):
+      if randint(1,1000) > 999:
+        data[ir][ic] += randint(1,3)
+      if c > 2:
+        data[ir][ic] -= c
+        if ir < len(data) -1:
+          data[ir+1][ic] += c
 
-    n = len(walls)
-    left_max = [0] * n
-    right_max = [0] * n
+def terminal_size():
+    th, tw, hp, wp = struct.unpack('HHHH',
+        fcntl.ioctl(0, termios.TIOCGWINSZ,
+        struct.pack('HHHH', 0, 0, 0, 0)))
+    return tw, th
 
-    # Calculate the maximum height of walls to the left of each position
-    left_max[0] = walls[0]
-    for i in range(1, n):
-        left_max[i] = max(left_max[i - 1], walls[i])
+width,height = terminal_size()
 
-    # Calculate the maximum height of walls to the right of each position
-    right_max[n - 1] = walls[n - 1]
-    for i in range(n - 2, -1, -1):
-        right_max[i] = max(right_max[i + 1], walls[i])
+data = []
+states = list(' .,:;|oO')
 
-    # Calculate the amount of water retained at each position
-    water_retained = 0
-    for i in range(n):
-        water_retained += max(0, min(left_max[i], right_max[i]) - walls[i])
+for _ in range(height):
+  row = []
+  for __ in range(width):
+    row.append(0)
+  data.append(row)
 
-    return water_retained
-
-# Test cases
-walls1 = [0, 1, 0, 2, 0, 3, 0, 4]
-walls2 = [2, 0, 0, 4, 0, 0, 1, 0]
-
-print(rain(walls1))  # Output: 6
-print(rain(walls2))  # Output: 6
+while True:
+  render()
+  update()
+  sleep(0.075)
