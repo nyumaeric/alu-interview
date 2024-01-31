639567705776094fc21c6aac9015b4a663e3ed04
nyumaeric <e.nyumajr@alustudent.com>
2024-01-31 07:19:33 +0000

    0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,43 +1,19 @@
-from time import sleep
-from random import randint
-import fcntl, termios, struct
+#!/usr/bin/python3
+""" calculate the rain"""
 
-def render():
-  print('\033[2J')
-  for r in data:
-    for c in r:
-      v = states[c] if c < len(states) else states[-1]
-      print(v,end='')
-    print('')
 
-def update():
-  for ir in range(len(data)-1,-1,-1):
-    for ic, c in enumerate(data[ir]):
-      if randint(1,1000) > 999:
-        data[ir][ic] += randint(1,3)
-      if c > 2:
-        data[ir][ic] -= c
-        if ir < len(data) -1:
-          data[ir+1][ic] += c
+def rain(walls):
+    if not walls or len(walls) < 3:
+        return 0
 
-def terminal_size():
-    th, tw, hp, wp = struct.unpack('HHHH',
-        fcntl.ioctl(0, termios.TIOCGWINSZ,
-        struct.pack('HHHH', 0, 0, 0, 0)))
-    return tw, th
+    total_water = 0
 
-width,height = terminal_size()
-
-data = []
-states = list(' .,:;|oO')
-
-for _ in range(height):
-  row = []
-  for __ in range(width):
-    row.append(0)
-  data.append(row)
-
-while True:
-  render()
-  update()
-  sleep(0.075)
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        total_water += min(left, right) - walls[i]
+    return total_water
